d688937ac39e839be763b5c8b77a129c89f048db
Xizhou-zzz <1158398445@qq.com>
2023-12-26 13:54:10 +0800

    exp6

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -2,7 +2,9 @@ use crate::task::{
     suspend_current_and_run_next,
     exit_current_and_run_next,
 };
+
 use crate::timer::get_time_ms;
+
 pub fn sys_exit(exit_code: i32) -> ! {
     println!("[kernel] Application exited with code {}", exit_code);
     exit_current_and_run_next();
@@ -13,7 +15,6 @@ pub fn sys_yield() -> isize {
     suspend_current_and_run_next();
     0
 }
-
 
 pub fn sys_get_time() -> isize {
     get_time_ms() as isize
